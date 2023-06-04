9becbbbc4712e0a55a10aeadde78fedcf154d0d8
Alex Berliner <alexberliner@gmail.com>
2023-06-04 01:58:57 -0700

    Build script for parser

---
+++ build.bat
@@ -0,0 +1,16 @@
+mkdir WoWTTS
+mkdir release
+mkdir release\QuestTextSender
+
+@REM build wow screen reader
+cd libserpix_rs
+cargo build --release --bin wow
+IF %ERRORLEVEL% NEQ 0 (
+    exit /b %ERRORLEVEL%
+)
+cd ..
+
+xcopy /E /H /C /I /Y QuestTextSender release\QuestTextSender
+xcopy /E /I /Y LibSerpix\LibSerpix release\QuestTextSender\Libs\LibSerpix
+copy libserpix_rs\target\release\wow.exe release\
+copy parser.py release\
